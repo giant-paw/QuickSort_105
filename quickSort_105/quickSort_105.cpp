966d31b7133ea45a690a19584f480cb8ab0d67ad
giant-paw <giantpawbusiness@gmail.com>
2023-03-30 08:16:51 +0700

    pembuatan void swap berfungsi guna memindahkan data dalam proses sorting

--- quickSort_105/quickSort_105.cpp
+++ quickSort_105/quickSort_105.cpp
@@ -28,6 +28,15 @@ void input() {
 	}
 }
 
+//swaps the element at index x with the element at index y
+void swap(int x, int y) {
+	int temp;
+
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
+
 int main() {
 	cout << endl;
 }
