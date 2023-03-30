1a11f79210dc95574615017333e0c2b5e2d489b4
giant-paw <giantpawbusiness@gmail.com>
2023-03-30 08:29:27 +0700

    pembuatan looping logika pivot

--- quickSort_105/quickSort_105.cpp
+++ quickSort_105/quickSort_105.cpp
@@ -46,6 +46,25 @@ void quick_sort(int low, int high) {
 
 	i = low + 1;
 	j = high;
+
+	while (i <= j) {
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) {
+			i++;
+			cmp_count++;
+		}
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) {
+			j--;
+			cmp_count++;
+		}
+		cmp_count++;
+
+		if (i < j) {					//if the greater element is on the left of the element
+			swap(i, j);					//swap the element at index i with the element at index
+			mov_count++;
+		}
+	}
 }
 
 int main() {
