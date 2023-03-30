e68126dcf676dc633ddb77b0ab77a62e572427db
giant-paw <giantpawbusiness@gmail.com>
2023-03-30 08:12:27 +0700

    Memasukkan data ke dalam array

--- quickSort_105/quickSort_105.cpp
+++ quickSort_105/quickSort_105.cpp
@@ -17,6 +17,15 @@ void input() {
 		else 
 			cout << "\nMaksimum Panjang Array Adalah 20 Element" << endl;
 	}
+
+	cout << "\n-------------------------------" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------------------" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[1];
+	}
 }
 
 int main() {
