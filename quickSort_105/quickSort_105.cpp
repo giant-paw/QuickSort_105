ca49c86262e0aee4a2812acc36e711d72a8735e6
giant-paw <giantpawbusiness@gmail.com>
2023-03-30 08:01:34 +0700

    Add project files.

---
+++ quickSort_105/quickSort_105.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0;		//number of comparisson
+int mov_count = 0;
+int n;
+
+int main() {
+	cout << endl;
+}
