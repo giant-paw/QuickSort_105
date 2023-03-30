d2b8e8c3c76758a89b40582eaa78d983ccd74a11
giant-paw <giantpawbusiness@gmail.com>
2023-03-30 08:06:55 +0700

    Pembuatan void input dan juga peng inputan panjang array

--- quickSort_105/quickSort_105.cpp
+++ quickSort_105/quickSort_105.cpp
@@ -7,6 +7,18 @@ int cmp_count = 0;		//number of comparisson
 int mov_count = 0;
 int n;
 
+void input() {
+	while (true) {
+		cout << "Masukkan Panjang Element Array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else 
+			cout << "\nMaksimum Panjang Array Adalah 20 Element" << endl;
+	}
+}
+
 int main() {
 	cout << endl;
 }
